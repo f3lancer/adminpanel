b784c9b63549b0ec915b9d0292d57db2fd33c5f8
f3lancer <116956300+f3lancer@users.noreply.github.com>
2024-03-25 04:13:58 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -16630,55 +16630,18 @@
     if (bacgroundmenu) bacgroundmenu.addEventListener("click", (function() {
         body.classList.toggle("openmobilemenu");
     }));
-    var myChart = echarts.init(document.getElementById("biggraff"));
-    window.addEventListener("resize", (function() {
-        myChart.resize();
-    }));
-    var script_option = {
-        title: {},
-        tooltip: {},
-        legend: {},
-        xAxis: {
-            data: [ "2023-2", "2023-4", "2023-6", "2023-8", "2023-10", "2023-12", "2024-2", "2024-4", "2024-6" ]
-        },
-        yAxis: {},
-        series: [ {
-            type: "bar",
-            data: [ 1600, 820, 1536, 1310, 1e3, 720, 500, 510, 800 ]
-        }, {
-            data: [ 1e3, 400, 300, 1550, 100, 720, 500, 510, 800 ],
-            type: "line",
-            smooth: true
-        } ]
+    const poapopen = document.querySelectorAll(".idc__popapon");
+    const contetn = document.querySelectorAll(".idc__popapcontent");
+    console.log(poapopen);
+    console.log(contetn);
+    for (let i = 0; i < poapopen.length; i++) poapopen[i].onclick = function() {
+        for (let v = 0; v < contetn.length; v++) {
+            contetn[v].classList.remove("active");
+            poapopen[v].classList.remove("active");
+        }
+        contetn[i].classList.add("active");
+        poapopen[i].classList.add("active");
     };
-    myChart.setOption(script_option);
-    var myChart1 = echarts.init(document.getElementById("main"));
-    var option2 = {
-        tooltip: {},
-        legend: {},
-        xAxis: {
-            data: [ "2023-2", "2023-4", "2023-6", "2023-8", "2023-10", "2023-12", "2024-2", "2024-4", "2024-6" ],
-            show: false
-        },
-        yAxis: {
-            show: false
-        },
-        series: [ {
-            label: {
-                formatter: [ "Label Text" ].join("\n"),
-                backgroundColor: "#eee",
-                borderColor: "#555",
-                fontSize: 18
-            },
-            data: [ 1e3, 400, 300, 1550, 100, 720, 500, 510, 800 ],
-            type: "line",
-            areaStyle: {
-                color: "#26FFE5",
-                opacity: .5
-            }
-        } ]
-    };
-    myChart1.setOption(option2);
     window["FLS"] = true;
     isWebp();
 })();
